c78426920d06f85935bb6e338b0945f31bfefdf7
ndayishimiyeeric <ndayishimiyeeric86@gmail.com>
2022-05-31 18:59:01 +0200

    Add react route and icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Home from './components/Home';
+import Quote from './components/Quote';
+import Navbar from './components/Navbar';
 
 class App extends React.Component {
   constructor(props) {
@@ -10,10 +14,12 @@ class App extends React.Component {
   render() {
     return (
       <div className="container">
-        <header className="title">
-          <h1>Math Magicians</h1>
-        </header>
-        <Calculator />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/quote" element={<Quote />} />
+          <Route path="/calculator" element={<Calculator />} />
+        </Routes>
       </div>
     );
   }
